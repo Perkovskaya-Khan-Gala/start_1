c40b5a386ff4413b4783b716bb6eaf0244a76825
Galka <hgalala@mail.ru>
2023-01-12 19:07:13 +0700

    Create folder Example with if branches

---
+++ Example_hi_if_Masha/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Input username:");
+string username=Console.ReadLine();
+if (username.ToLower()=="masha")
+{
+    Console.WriteLine("Hey! This is Masha");
+}
+else
+{
+    Console.Write("Hello,");
+    Console.WriteLine(username);
+}
